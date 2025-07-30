633e6fd80de83ced971f4b0fdff7b528a4749985
thomas-roth <uruox@student.kit.edu>
2025-07-30 11:27:03 +0200

    fix final logs location, remove verbose flag in logs rsync

--- sbatch_train_calvin.sh
+++ sbatch_train_calvin.sh
@@ -39,4 +39,4 @@ srun python /home/hk-project-p0024638/uruox/code/iTRAP/iTRAP/models/flower_vla_c
 # batch_size=2 model=smolflow_agent # batch_size=4 model.use_lora=False # model=vlm_berg_agent # batch_size=2 model.vla_mode='reduced_head' #model.use_perceiver=False model.use_incontext=True #seed=242 #model=mode_agent
 
 # Sync logs of slurm & run to workspace
-rsync -av $TMPDIR/logs /hkfs/work/workspace/scratch/uruox-itrap_flower_abc/logs
+rsync -a $TMPDIR/logs /hkfs/work/workspace/scratch/uruox-itrap_flower_abc
