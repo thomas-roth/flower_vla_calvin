8f9b865c2f72c1789175815421eccd553afebffe
thomas-roth <uruox@student.kit.edu>
2025-10-27 19:45:53 +0100

    change vlm task input from shorthand form to text description (like in ds), change config to best current flower checkpoint

--- flower/rollout/rollout_long_horizon.py
+++ flower/rollout/rollout_long_horizon.py
@@ -312,7 +312,7 @@ class RolloutLongHorizon(Callback):
 
         # get trajectory points & actions from initial state of scene & robot (static camera image untransformed as render() used instead of get_obs())
         untransformed_static_img = self.env.cameras[0].render()[0].squeeze()
-        vlm_response = query_vlm(untransformed_static_img, vlm_client, subtask)
+        vlm_response = query_vlm(untransformed_static_img, vlm_client, goal["lang_text"])
         traj_gripper_points, traj_gripper_actions = extract_gripper_points_and_actions(vlm_response, untransformed_static_img.shape[0], untransformed_static_img.shape[1],
                                                                                        error_logger=log_print, stretch_factor=self.traj_stretch_factor)
 
@@ -333,7 +333,7 @@ class RolloutLongHorizon(Callback):
             if step == self.ep_len / 2:
                 # query_vlm again to help robot out of possibly wrong state
                 untransformed_static_img = self.env.cameras[0].render()[0].squeeze()
-                vlm_response = query_vlm(untransformed_static_img, vlm_client, subtask)
+                vlm_response = query_vlm(untransformed_static_img, vlm_client, goal["lang_text"])
                 traj_gripper_points, traj_gripper_actions = extract_gripper_points_and_actions(vlm_response, untransformed_static_img.shape[0], untransformed_static_img.shape[1],
                                                                                                error_logger=log_print, stretch_factor=self.traj_stretch_factor)
             
